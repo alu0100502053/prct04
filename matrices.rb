70e5b95ef05a76dd71e476e91c7a4386012c4760
Killian Jimenez <alu0100502053@ull.edu.es>
2013-10-05 19:03:17 +0100

    Corregida estética menu

--- matrices.rb
+++ matrices.rb
@@ -1,16 +1,28 @@
 require 'matrix'
 
+def suma
+   puts "Suma"
+end
+
+def multiplicacion
+   puts "Multiplicacion"
+end
+
 def menu
-   puts "Menu"
-   puts ""
-   puts "1) Sumar Matrices"
-   puts "2) Multiplicar Matrices"
-   puts "0) Salir"
-   puts 
-   print "Opcion: "
-   opc = gets.to_i
-   suma if opc == 1
-   multiplicacion if opc == 2
+   opc = ''
+   while opc != 0 do
+      puts "            Menu"
+      puts "-----------------------------"
+      puts "-- 1) Sumar Matrices       --"
+      puts "-- 2) Multiplicar Matrices --"  
+      puts "-- 0) Salir                --"
+      puts "-----------------------------"     
+      puts 
+      print "Opcion: "
+      opc = gets.to_i
+      suma if opc == 1
+      multiplicacion if opc == 2
+   end
 end
 
 menu
